26d4c9783fcb2a57f03ac5d814a025cb7207e757
JOHN MBITHI MUTAVE <83606270+Jellyjones-pixel@users.noreply.github.com>
2023-09-27 16:32:26 +0100

    Add files via upload
    Updated

---
+++ SwitchStatementExample1/src/switchstatementexample1/SwitchStatementExample1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package switchstatementexample1;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class SwitchStatementExample1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int a = 10; 
+// Evaluating the expression that will return true or false 
+switch(a){ 
+case 1: 
+System.out.println("Value of a: 1");
+break;
+case 5: 
+System.out.println("Value of a: 5");
+break; 
+case 10: 
+System.out.println("Value of a: 10");
+break; 
+default: 
+System.out.println("else block"); 
+break; 
+} 
+
+    }
+    
+}
